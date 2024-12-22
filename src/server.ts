8c5b9ec99353034798c1c789501a73ed1186c612
James Stevens <jsteve1@stanford.edu>
2024-12-22 15:55:13 -0500

    server setup

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,9 @@ app.get('/', (req, res) => {
 
 app.use('/api', coinbaseRoutes);
 
-// Only start the server if we're not in Lambda
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5001;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
